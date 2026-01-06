caff219db39f412ed0ccc838203f36d29ff88d45
Aadarsh Gupta <prathamtogupta11@gmail.com>
2026-01-06 19:18:45 +0530

    logo little updated

--- src/app/components/Navbar/Logo.jsx
+++ src/app/components/Navbar/Logo.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Logo = () => {
   return (
-    <div>
-      <span className="font-semibold">Char Pahiya</span>
+    <div className="font-semibold text-gray-900">
+      <span className="px-2 py-1 border border-gray-300 rounded">
+        Car Vishwas
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
